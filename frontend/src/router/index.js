9dc0427646cbd4d882433e4f5193dcbaba9b8a55
danielleirishmaymartinez <danielleirishmaymartinez@gmail.com>
2025-11-13 09:05:08 +0800

    adding files

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+import LandingPage from "@/views/LandingPage.vue";
+import AboutPage from "@/views/AboutPage.vue";
+import UploadPage from "@/views/UploadPage.vue";
+import AnalyzingPage from "@/views/AnalyzingPage.vue";
+import ResultPage from "@/views/ResultPage.vue";
+
+const routes = [
+  { path: "/", name: "Landing", component: LandingPage },
+  { path: "/about", name: "About", component: AboutPage },
+  { path: "/upload", name: "Upload", component: UploadPage },
+  { path: "/analyzing", name: "Analyzing", component: AnalyzingPage },
+  { path: "/results", name: "Results", component: ResultPage },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
