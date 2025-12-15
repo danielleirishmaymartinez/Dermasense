28929d47aadaf58e719aa924be7fbf8224a2bac3
Leslie Mae Pelletero <lesliemaepelletero@gmail.com>
2025-12-15 10:09:48 +0800

    fix

--- backend/main.py
+++ backend/main.py
@@ -250,5 +250,5 @@ async def predict(
         raise HTTPException(status_code=500, detail=f"Unexpected error: {e}")
 
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
